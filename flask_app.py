3bda6fadaf5253af769ef792cbd791c9245eab87
Jeremy Drake <github@jdrake.com>
2022-04-09 20:20:45 -0700

    use pygithub instead of github-flask

--- flask_app.py
+++ flask_app.py
@@ -3,13 +3,14 @@
 
 import functools
 import hmac
+import requests
 import secrets
 
 from contextlib import contextmanager
 
 from cryptography.fernet import Fernet
 from flask import Flask, request, abort, session, url_for, redirect
-from flask_github import GitHub as FlaskGitHub
+from github import Github
 
 
 @contextmanager
@@ -25,7 +26,7 @@ app = Flask(__name__, instance_relative_config=True)
 # XXX do I care about default settings?
 app.config.from_pyfile('application.cfg', silent=True)
 
-flaskgithub = FlaskGitHub(app)
+oauthapp = Github().get_oauth_application(app.config["GITHUB_CLIENT_ID"], app.config["GITHUB_CLIENT_SECRET"])
 
 def encrypt_protected_var(cleartext: str) -> str:
     return Fernet(app.config['FERNET_SECRET_KEY'].encode('utf-8')).encrypt(cleartext.encode('utf-8')).decode('utf-8')
@@ -50,8 +51,7 @@ def github_oauth_state_check(func):
     @functools.wraps(func)
     def wrapper_github_oauth_state_check(*args, **kwargs):
         if 'auth_state' in session:
-            auth_state = session.pop('auth_state')
-            if not secrets.compare_digest(auth_state, request.args['state']):
+            if not secrets.compare_digest(session['auth_state'], request.args['state']):
                 return abort(401, 'Bad CSRF token')
         return func(*args, **kwargs)
     return wrapper_github_oauth_state_check
@@ -65,7 +65,7 @@ def index():
 
 @app.route("/github-webhook", methods=['POST'])
 @verify_github_webhook_signature
-def githubWebhook():
+def github_webhook():
     return "Got it"
 
 @app.route('/login')
@@ -74,7 +74,7 @@ def login():
         return "Already logged in!"
     else:
         session['auth_state'] = secrets.token_urlsafe()
-        return flaskgithub.authorize(state=session['auth_state'])
+        return redirect(oauthapp.get_login_url(state=session['auth_state']))
 
 @app.route('/logout')
 def logout():
@@ -82,9 +82,9 @@ def logout():
     next_url = request.args.get('next') or url_for('index')
     if 'user_access_token' in session:
         access_token=decrypt_protected_var(session['user_access_token'])
-        resp = flaskgithub.session.delete(f'{flaskgithub.BASE_URL}applications/{app.config["GITHUB_CLIENT_ID"]}/token',
-                                   data=f'{{"access_token":"{access_token}"}}',
-                                   auth=(app.config["GITHUB_CLIENT_ID"], app.config["GITHUB_CLIENT_SECRET"]))
+        resp = requests.delete(f'https://api.github.com/applications/{app.config["GITHUB_CLIENT_ID"]}/token',
+                               data=f'{{"access_token":"{access_token}"}}',
+                               auth=(app.config["GITHUB_CLIENT_ID"], app.config["GITHUB_CLIENT_SECRET"]))
     [session.pop(key) for key in list(session.keys()) if key.startswith('user_')]
     if resp is not None:
         resp.raise_for_status()
@@ -92,21 +92,17 @@ def logout():
 
 @app.route('/github-callback')
 @github_oauth_state_check
-@flaskgithub.authorized_handler
-def authorized(access_token):
+def authorized():
     next_url = request.args.get('next') or url_for('index')
 
-    if access_token is None:
-        return redirect(next_url)
+    access_token = oauthapp.get_access_token(request.args['code'], session.get('state'))
 
-    session['user_access_token'] = encrypt_protected_var(access_token)
+    session['user_access_token'] = encrypt_protected_var(access_token.token)
 
-    # TODO use pygithub instead
-    with temporary_attribute(flaskgithub, 'get_access_token', lambda *a: access_token):
-        github_user = flaskgithub.get('/user')
-        session['user_id'] = github_user['id']
-        session['user_type'] = github_user['type']
-        session['user_login'] = github_user['login']
+    user = Github(access_token.token).get_user()
+    session['user_id'] = user.id
+    session['user_type'] = user.type
+    session['user_login'] = user.login
 
     return redirect(next_url)
 
