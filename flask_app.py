1abe744f30198249804c04672ff7195760c37eba
Jeremy Drake <github@jdrake.com>
2022-04-11 04:21:30 +0000

    clear login info from session if token isn't valid

--- flask_app.py
+++ flask_app.py
@@ -36,6 +36,9 @@ def encrypt_protected_var(cleartext: str) -> str:
 def decrypt_protected_var(ciphertext: str) -> str:
     return Fernet(app.config['FERNET_SECRET_KEY'].encode('utf-8')).decrypt(ciphertext.encode('utf-8')).decode('utf-8')
 
+def clear_login_session():
+    [session.pop(key) for key in list(session.keys()) if key.startswith('user_')]
+
 def handle_login(next=None):
     session['auth_state'] = secrets.token_urlsafe()
     if next:
@@ -77,6 +80,7 @@ def verify_login_token(func):
                                  data=f'{{"access_token":"{access_token}"}}',
                                  auth=(app.config["GITHUB_CLIENT_ID"], app.config["GITHUB_CLIENT_SECRET"]))
         if not resp:
+            clear_login_session()
             return handle_login(url_for(request.endpoint, **request.view_args))
         return func(*args, **kwargs)
     return wrapper_verify_login_token
@@ -122,7 +126,7 @@ def logout():
         requests.delete(f'https://api.github.com/applications/{app.config["GITHUB_CLIENT_ID"]}/token',
                         data=f'{{"access_token":"{access_token}"}}',
                         auth=(app.config["GITHUB_CLIENT_ID"], app.config["GITHUB_CLIENT_SECRET"]))
-    [session.pop(key) for key in list(session.keys()) if key.startswith('user_')]
+    clear_login_session()
     return redirect(url_for('index'))
 
 @app.route('/github-callback')
